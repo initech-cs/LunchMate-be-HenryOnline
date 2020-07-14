ffd80db5f85c7418921ef570da31617a5ea55d13
fool1280 <anhnguyen@Anhs-MBP.attlocal.net>
2020-07-14 02:06:30 -0500

    default master

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("Everything is ok.");
+  res.send("LunchMate Backend Works.");
 });
 
 module.exports = router;
